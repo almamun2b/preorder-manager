c692728e63e62006544ae44f261d061e29604456
almamun2b <almamun2b@gmail.com>
2026-06-26 01:58:38 +0600

    Turso Remote SqLite DB Setup

--- src/backend/config/prisma.ts
+++ src/backend/config/prisma.ts
@@ -1,9 +1,12 @@
+// import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 import { PrismaClient } from '@/generated/prisma/client'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 
 const dbUrl = `${process.env.DATABASE_URL}`
+const authToken = `${process.env.DATABASE_AUTH_TOKEN}`
 
-const adapter = new PrismaBetterSqlite3({ url: dbUrl })
+// const adapter = new PrismaBetterSqlite3({ url: dbUrl })
+const adapter = new PrismaLibSql({ url: dbUrl, authToken })
 
 const prisma = new PrismaClient({
   log:
